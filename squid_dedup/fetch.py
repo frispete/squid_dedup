efa1ee4a980cbb3692667d48a33b46088460b62a
Hans-Peter Jansen <hpj@urpla.net>
2016-05-03 00:00:29 +0200

    squid_dedup/fetch.py:  - add stop event

--- squid_dedup/fetch.py
+++ squid_dedup/fetch.py
@@ -11,27 +11,40 @@ import threading
 
 log = logging.getLogger('fetch')
 
+BLOCKSIZE = 8192
+
 class Fetch(threading.Thread):
     """ fetch objects from queue """
     def __init__(self, queue):
         self._queue = queue
         super().__init__()
+        self._stop = threading.Event()
+        self.setDaemon(True)
+
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
 
     def run(self):
-        while True:
+        while not self.stopped():
             url = self._queue.get()
-            if url is None:
-                break
-            log.debug('fetch <%s>', url)
+            log.debug('%s: fetch <%s>', self.name, url)
             try:
                 response = urllib.request.urlopen(url)
             except urllib.error.URLError as e:
                 log.error('open <%s> failed: %s', url, e)
             else:
                 log.trace(response.info())
-                try:
-                    response.read()
-                except Exception as e:
-                    log.error('read <%s> failed: %s', url, e)
-                else:
+                while not self.stopped():
+                    try:
+                        data = response.read(BLOCKSIZE)
+                    except Exception as e:
+                        log.error('read <%s> failed: %s', url, e)
+                    else:
+                        if not data:
+                            break
+                if not self.stopped():
                     log.info('fetched <%s>', url)
+        log.debug('%s: fetch stopped', self.name)
