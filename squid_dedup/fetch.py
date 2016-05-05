7b69556e75ad3068acf2cd572b88d7597f510aa8
Hans-Peter Jansen <hpj@urpla.net>
2016-05-05 21:07:17 +0200

    squid_dedup/fetch.py:  - add proxy support (helper get a sanitized environment without proxy settings)  - keep track with fetched objects  - don't fetch cached objects again

--- squid_dedup/fetch.py
+++ squid_dedup/fetch.py
@@ -4,10 +4,12 @@
 # License: GNU GPL 2 - see http://www.gnu.org/licenses/gpl2.txt for details
 # vim:set et ts=8 sw=4:
 
+import time
 import queue
 import urllib
 import urllib.request
 import logging
+from collections import defaultdict
 
 log = logging.getLogger('fetch')
 
@@ -15,11 +17,27 @@ BLOCKSIZE = 8192
 QUEUE_TIMEOUT = 0.5
 
 class Fetch:
+
+    _done = defaultdict(set)
+
     """ fetch objects from queue """
     def __init__(self, config, queue):
         self._config = config
         self._queue = queue
         self._exiting = False
+
+        self._delay = config.fetch_delay
+
+        # prepare proxies
+        proxies = {}
+        if self._config.http_proxy:
+            proxies['http'] = self._config.http_proxy
+        if self._config.https_proxy:
+            proxies['https'] = self._config.https_proxy
+        if proxies:
+            proxy_support = urllib.request.ProxyHandler(proxies)
+            opener = urllib.request.build_opener(proxy_support)
+            urllib.request.install_opener(opener)
 
     def exit(self):
         self._exiting = True
@@ -28,16 +46,33 @@ class Fetch:
         log.debug('%s: running', name)
         while not self._exiting:
             try:
-                url = self._queue.get(timeout = QUEUE_TIMEOUT)
+                newurl, url = self._queue.get(timeout = QUEUE_TIMEOUT)
             except queue.Empty:
                 continue
-            log.debug('%s: <%s>', name, url)
+            log.debug('%s: %s, %s', name, newurl, url)
+            if newurl in Fetch._done:
+                Fetch._done[newurl].add(url)
+                continue
+            Fetch._done[newurl].add(url)
+            time.sleep(self._delay)
             try:
                 response = urllib.request.urlopen(url)
             except urllib.error.URLError as e:
                 log.error('%s: open <%s> failed: %s', name, url, e)
             else:
-                log.trace('%s: %s', name, response.info())
+                # check, if object is cached already
+                header = response.info()
+                log.info('%s: %s\n%s', name, url, header)
+                try:
+                    xcache = header['X-Cache']
+                except KeyError:
+                    pass
+                else:
+                    if xcache.startswith('HIT'):
+                        log.info('%s: %s is cached already', name, url)
+                        continue
+                # object isn't fetched already, do it now
+                log.info('%s: fetching %s', name, url)
                 while not self._exiting:
                     try:
                         data = response.read(BLOCKSIZE)
